eba7d3b7934e45abc6f80f342780e414db1dbab3
charbel-2 <131307737+charbel-2@users.noreply.github.com>
2026-06-23 17:39:26 +0200

    Update InteractionMetaModel_Physics_train.py

--- main_paper_codes/InteractionMetaModel_Physics_train.py
+++ main_paper_codes/InteractionMetaModel_Physics_train.py
@@ -295,7 +295,7 @@ try:
         "R": criterion.R
     }
 
-    model = EnhancedTransformer(input_dim=18, n_heads=num_heads, n_layers=8, n_embd=embed_dim, forward_expansion=6, 
+    model = EnhancedTransformer(input_dim=18, n_heads=num_heads, n_layers=6, n_embd=embed_dim, forward_expansion=6, 
                             seq_len= seq_length,seq_len_dec= len_dec, mean= relevant_mean, std= relevant_std, physics_params= physics_params) .to(device)
 
     wandb.init(project="meta-Franka-PhysicsInformed", name=f"Physics_Informed_train", reinit=True)
